2401410d0d0138cb689de93ee244bb7f3a9b802f
Meike Vocke <meike.vocke@avionic-design.de>
2016-07-25 08:58:37 +0200

    Add developer information
    Add developer's name and e-mail address to each source file.
    
    Change-Id: Ifdb97f34e32542c7f9d5e029d9dd624cc8484ecb
    Signed-off-by: Meike Vocke <meike.vocke@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6090
    Reviewed-by: Bert van Hall <bert.vanhall@avionic-design.de>

--- include/camera_device.h
+++ include/camera_device.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 Avionic Design GmbH
+ * Meike Vocke <meike.vocke@avionic-design.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
